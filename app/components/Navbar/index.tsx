25715c28b840e4234a9e580b8873d36e12df21d1
Elian de Oliveira <87070440+elianoli@users.noreply.github.com>
2024-01-04 15:59:53 -0300

    not working responsive navbar

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,18 +1,45 @@
+"use client";
+
+import { useState } from "react";
+
 const Navbar = () => {
+  const TOGGLE_START: boolean = false;
+
+  const [toggle, setToggle] = useState(TOGGLE_START);
+
   return (
     <>
       <nav className="nav">
         <h2 className="text-3xl text-white">
           <a href="#">./&nbsp;elian</a>
         </h2>
-        <div className="hidden md:block">
+        <div id="menu" className="hidden md:block">
           <ul className="links">
             <a href="#">About</a>
             <a href="#">Projects</a>
             <a href="#">Contact</a>
           </ul>
         </div>
-        <button className="btn">Hire&nbsp;me</button>
+        <button className="btn" onClick={() => setToggle(!toggle)}>
+          Hire&nbsp;me
+        </button>
+        <svg
+          id="menu-button"
+          xmlns="<http://www.w3.org/2000/svg>"
+          className={`h-8 w-8 cursor-pointer md:${
+            toggle ? "block" : "hidden"
+          } block`}
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#fff"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
       </nav>
     </>
   );
